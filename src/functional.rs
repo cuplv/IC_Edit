a8ab9f792a1abab8768d904f5a8ccf6178a5c6ac
Kyle Headley <kyle.headley@colorado.edu>
2015-12-01 19:32:54 -0700

    prep for multi-line text

--- src/functional.rs
+++ src/functional.rs
@@ -5,112 +5,149 @@
 use std::rc::Rc;
 
 pub struct List<T> {
-    head: Link<T>,
+  head: Link<T>,
 }
 
 type Link<T> = Option<Rc<Node<T>>>;
 
 struct Node<T> {
-    elem: T,
-    next: Link<T>,
+  elem: T,
+  next: Link<T>,
 }
 
 pub struct Iter<'a, T:'a> {
-    next: Option<&'a Node<T>>,
+  next: Option<&'a Node<T>>,
 }
 
 
 
 
 impl<T> List<T> {
-    pub fn new() -> Self {
-        List { head: None }
-    }
+  pub fn new() -> Self {
+    List { head: None }
+  }
 
-    pub fn empty(&self) -> bool {
-        if let Some(_) = self.head {true} else {false}
-    }
+  pub fn empty(&self) -> bool {
+    if let Some(_) = self.head {true} else {false}
+  }
 
-    pub fn append(&self, elem: T) -> List<T> {
-        List { head: Some(Rc::new(Node {
-            elem: elem,
-            next: self.head.clone(),
-        }))}
-    }
+  pub fn append(&self, elem: T) -> List<T> {
+    List { head: Some(Rc::new(Node {
+      elem: elem,
+      next: self.head.clone(),
+    }))}
+  }
 
-    pub fn tail(&self) -> List<T> {
-        List { head: self.head.as_ref().and_then(|node| node.next.clone()) }
-    }
+  pub fn tail(&self) -> List<T> {
+    List { head: self.head.as_ref().and_then(|node| node.next.clone()) }
+  }
 
-    pub fn head(&self) -> Option<&T> {
-        self.head.as_ref().map(|node| &node.elem)
-    }
+  pub fn head(&self) -> Option<&T> {
+    self.head.as_ref().map(|node| &node.elem)
+  }
 
-    pub fn iter(&self) -> Iter<T> {
-        Iter { next: self.head.as_ref().map(|node| &**node) }
+  pub fn iter(&self) -> Iter<T> {
+    Iter { next: self.head.as_ref().map(|node| &**node) }
+  }
+}
+
+impl<T: Clone> List<T> {
+  //TODO: If possible, better use of '&' and 'ref' here
+  pub fn rev(&self) -> List<T> {
+    fn rev<E: Clone>(&ref i: &Link<E>, o: Link<E>) -> Link<E> {
+      match i {
+        &None => {o}
+        &Some(ref n) => {
+          rev(&n.next, Some(Rc::new(Node {
+            elem: n.elem.clone(),
+            next: o
+          })))
+        }
+      }
     }
+    List { head: rev(&self.head, None) }
+  }
 }
 
 impl<T> Drop for List<T> {
-    fn drop(&mut self) {
-        let mut head = self.head.take();
-        while let Some(node) = head {
-            if let Ok(mut node) = Rc::try_unwrap(node) {
-                head = node.next.take();
-            } else {
-                break;
-            }
-        }
+  fn drop(&mut self) {
+    let mut head = self.head.take();
+    while let Some(node) = head {
+      if let Ok(mut node) = Rc::try_unwrap(node) {
+        head = node.next.take();
+      } else {
+        break;
+      }
     }
+  }
 }
 
 impl<'a, T> Iterator for Iter<'a, T> {
-    type Item = &'a T;
+  type Item = &'a T;
 
-    fn next(&mut self) -> Option<Self::Item> {
-        self.next.map(|node| {
-            self.next = node.next.as_ref().map(|node| &**node);
-            &node.elem
-        })
-    }
+  fn next(&mut self) -> Option<Self::Item> {
+    self.next.map(|node| {
+      self.next = node.next.as_ref().map(|node| &**node);
+      &node.elem
+    })
+  }
 }
 
 
 
 #[cfg(test)]
 mod test {
-    use super::List;
+  use super::List;
 
-    #[test]
-    fn basics() {
-        let list = List::new();
-        assert_eq!(list.head(), None);
+  #[test]
+  fn basics() {
+    let list = List::new();
+    assert_eq!(list.head(), None);
 
-        let list = list.append(1).append(2).append(3);
-        assert_eq!(list.head(), Some(&3));
+    let list = list.append(1).append(2).append(3);
+    assert_eq!(list.head(), Some(&3));
 
-        let list = list.tail();
-        assert_eq!(list.head(), Some(&2));
+    let list = list.tail();
+    assert_eq!(list.head(), Some(&2));
 
-        let list = list.tail();
-        assert_eq!(list.head(), Some(&1));
+    let list = list.tail();
+    assert_eq!(list.head(), Some(&1));
 
-        let list = list.tail();
-        assert_eq!(list.head(), None);
+    let list = list.tail();
+    assert_eq!(list.head(), None);
 
-        // Make sure empty tail works
-        let list = list.tail();
-        assert_eq!(list.head(), None);
+    // Make sure empty tail works
+    let list = list.tail();
+    assert_eq!(list.head(), None);
 
-    }
+  }
 
-    #[test]
-    fn iter() {
-        let list = List::new().append(1).append(2).append(3);
+  #[test]
+  fn iter() {
+    let list = List::new().append(1).append(2).append(3);
 
-        let mut iter = list.iter();
-        assert_eq!(iter.next(), Some(&3));
-        assert_eq!(iter.next(), Some(&2));
-        assert_eq!(iter.next(), Some(&1));
-    }
+    let mut iter = list.iter();
+    assert_eq!(iter.next(), Some(&3));
+    assert_eq!(iter.next(), Some(&2));
+    assert_eq!(iter.next(), Some(&1));
+  }
+
+  #[test]
+  fn rev() {
+    let list = List::new().append(1).append(2).append(3);
+
+    let list = list.rev();
+    assert_eq!(list.head(), Some(&1));
+
+    let list = list.tail();
+    assert_eq!(list.head(), Some(&2));
+
+    let list = list.tail();
+    assert_eq!(list.head(), Some(&3));
+    
+    let list = list.tail();
+    assert_eq!(list.head(), None);
+    
+  }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -21,48 +21,85 @@ const OPEN_GL: OpenGL = OpenGL::V3_2;
 type Cursor = String;
 
 #[derive(Debug)]
+enum Dir {
+  L,
+  R,
+}
+
+#[derive(Debug)]
 enum Symbol {
 	Cur(Cursor),
 	Data(String),
 }
 
-//struct Zip;
-
-fn build_content(keys: &List<Symbol>) -> String {
-  fn build(ks: &List<Symbol>, c: String) -> String {
-    if let Some(k) = ks.head() {
-      match k {
-        &Symbol::Cur(_) => build(&ks, c),
-        //TODO: find a more elegant way to copy s
-        &Symbol::Data(ref s) => build(&ks.tail(), s.to_string() + &c)
-      }
-    } else {c}
-  }
-  build(keys, "".to_string())
+#[derive(Debug)]
+enum Action {
+  Cmd(Command),
+  Undo,
+  Redo,
 }
 
-// Returns a result containing a GlutinWindow or an error if the window
-// settings are not supported
-fn try_create_window() -> Result<GlutinWindow, String> {
-  WindowSettings::new("ICEdit", [800, 600])
-    .exit_on_esc(true)
-    .opengl(OPEN_GL)
-    .build()
+#[derive(Debug)]
+enum Command {
+  Ins(String, Dir),
+  Rem(Dir),
+  Move(Dir),
+  Repl(String, Dir),
+  Mk(Cursor),
+  Switch(Cursor),
+  Jmp(Cursor),
+  Join(Cursor),
 }
 
-fn render(c: graphics::context::Context, g: &mut GlGraphics, f: &mut GlyphCache, t: &String) {
+type Zip<T> = (List<T>,List<T>);
+type CZip<T> = (List<T>,Cursor,List<T>);
+
+fn build_content(keys: &List<Action>) -> List<String> {
+  println!("Produce text"); "".to_string();
+  // fn build(ks: &List<Symbol>, c: String) -> String {
+  //   if let Some(k) = ks.head() {
+  //     match k {
+  //       &Symbol::Cur(_) => build(&ks, c),
+  //       //TODO: find a more elegant way to copy s
+  //       &Symbol::Data(ref s) => build(&ks.tail(), s.to_string() + &c)
+  //     }
+  //   } else {c}
+  // }
+  // build(keys, "".to_string())
+  List::new()
+    .append("nothing here".to_string())
+    .append("or here".to_string())
+    .rev()
+}
+
+fn render(c: graphics::context::Context, g: &mut GlGraphics, f: &mut GlyphCache, t: &List<String>) {
   graphics::clear([0.0, 0.0, 0.0, 1.0], g);
 
   //println!("{:?}", &t);
 
   let mut text = graphics::Text::new(22);
-  text.color = [1.0, 0.0, 0.0, 1.0];
-  text.draw(
-    &t,
-    f,
-    &c.draw_state,
-    c.trans(10.0, 20.0).transform,
-    g);
+  text.color = [1.0, 1.0, 1.0, 1.0];
+  let mut loc = 20.0;
+
+  for st in t.iter() {
+    text.draw(
+      st,
+      f,
+      &c.draw_state,
+      c.trans(10.0, loc).transform,
+      g); 
+    loc += 20.0;
+    if loc > 800.0 {break}
+  }
+}
+
+// Returns a result containing a GlutinWindow or an error if the window
+// settings are not supported
+fn try_create_window() -> Result<GlutinWindow, String> {
+  WindowSettings::new("ICEdit", [800, 800])
+    .exit_on_esc(true)
+    .opengl(OPEN_GL)
+    .build()
 }
 
 fn main() {
@@ -75,14 +112,14 @@ fn main() {
   let mut needs_update = true;
   let mut command_key_down = false;
   let mut inputs = List::new();
-  let mut content_text = "".to_string();
+  let mut content_text = List::new().append("".to_string());
 
   for e in window.events().max_fps(60) {
     match e {
       //gives typed char or empty
       Event::Input(Input::Text(t)) => {
         if t == "" || command_key_down {continue}
-        inputs = inputs.append(Symbol::Data(t));
+        inputs = inputs.append(Action::Cmd(Command::Ins(t,Dir::L)));
         needs_update = true;
       }
       Event::Input(Input::Release(Button::Keyboard(key))) => {
@@ -111,23 +148,43 @@ fn main() {
           Key::Left |
           Key::Right |
           Key::Up |
-          Key::Down |
-          Key::Backspace  => {
+          Key::Down => {
             let m = if command_key_down {"C: "} else {""};
             println!("{}{:?}", m, key);
-            needs_update = true;
+          }
+          Key::Backspace  => {
+            if command_key_down {println!("C: Backspace");}
+            else{
+              inputs = inputs.append(
+                Action::Cmd(Command::Rem(Dir::L))
+              );
+              needs_update = true;
+            }
           }
           Key::Return => {
             if command_key_down {println!("C: Return");}
             else {
-              inputs = inputs.append(Symbol::Data("\n".to_string()));
+              inputs = inputs.append(
+                Action::Cmd(Command::Ins("\n".to_string(), Dir::L))
+              );
               needs_update = true;
             }
+          }
+          Key::Z => {
+            if command_key_down {
+              inputs = inputs.append(Action::Undo);
+              needs_update = true;
+            } else {continue}
+          }
+          Key::Y => {
+            if command_key_down {
+              inputs = inputs.append(Action::Redo);
+              needs_update = true;
+            } else {continue}
           }
           _ => {
             if command_key_down {
               println!("C: {:?}", key);
-              needs_update = true;
             }
           }
         }
