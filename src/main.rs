c2e0bbdbd1e2718c77543c826803d055d4c152ad
Matthew Hammer <matthew.hammer@colorado.edu>
2016-01-05 12:15:58 -0700

    moved spec into another module

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,7 @@ extern crate piston;
 extern crate adapton;
 
 mod functional;
+mod spec;
 
 use std::env::current_exe;
 use time::Duration;
@@ -41,269 +42,9 @@ use piston::input::{Button, Event, Input, Key};
 use piston::window::WindowSettings;
 use functional::List;
 
+use spec::*;
+
 const OPEN_GL: OpenGL = OpenGL::V3_2;
-
-type Cursor = String;
-
-#[derive(Debug, Clone)]
-enum Dir {
-  L,
-  R,
-}
-
-impl Dir {
-  fn opp(&self) -> Dir {
-    match *self {
-      Dir::L => {Dir::R}
-      Dir::R => {Dir::L}
-    }
-  }
-}
-
-#[derive(Debug, Clone)]
-enum Symbol {
-	Cur(Cursor),
-	Data(String),
-}
-
-#[derive(Debug, Clone)]
-enum Action {
-  Cmd(Command),
-  Undo,
-  Redo,
-}
-
-#[derive(Debug, Clone)]
-enum Command {
-  Ins(String, Dir),   //Insert <String>, moving cursor <Dir>
-  Rem(Dir),           //Remove character located <Dir>
-  Move(Dir),          //Move cursor <Dir>
-  Ovr(String, Dir),   //Overwrite with <String>, moving cursor <Dir>
-  Mk(Cursor),
-  Switch(Cursor),
-  Jmp(Cursor),
-  Join(Cursor),
-}
-
-#[derive(Debug, Clone)]
-enum CCs {
-  Mk, Switch, Jmp, Join
-}
-
-enum Inputstatus {
-  Insert(Dir, bool),    //Mode, direction, showcursors
-  Overwrite(Dir, bool),
-  EnterCursor(
-    Box<Inputstatus>,   // prior input status
-    CCs,                // current command in process
-    List<Action>,       // actions generating new cursor
-    String,             // new cursor in progress
-  )
-}
-
-type Zip<T> = (List<T>,List<T>);
-type CZip<T> = (List<T>,Cursor,List<T>);
-
-//Action list to undo buffer
-fn al_to_ub(acts: &List<Action>) -> Zip<Command> {
-  let mut content: List<Command> = List::new();
-  let mut buffer: List<Command> = List::new();
-
-  for act in acts.iter() {
-    let (content2, buffer2) =
-    match *act {
-      Action::Undo => {
-        match content.head() {
-          None => {(List::new(),buffer)}
-          Some(d) => {
-            (content.tail(),
-              buffer.append(d.clone()))
-          }
-        }
-      },
-      Action::Redo => {
-        match buffer.head() {
-          None => {(content,List::new())}
-          Some(d) => {
-            (content.append(d.clone()),
-              buffer.tail())
-          }
-        }
-      },
-      Action::Cmd(ref c) => {
-        (content.append(c.clone()),
-          List::new())
-      }
-    };
-    content = content2;
-    buffer = buffer2;
-  }
-  (content,buffer)
-}
-
-//move the zipper through cursors in the given direction 
-fn passthrough(direction: Dir, before: List<Symbol>, after: List<Symbol>)
-  -> (List<Symbol>, List<Symbol>) {
-  let mut head;
-  let mut first;
-  let mut second;
-
-  match direction {
-    Dir::L => {
-      first = before;
-      second = after;
-    }
-    Dir::R => {
-      first = after;
-      second = before;
-    }
-  }
-
-  loop {
-    head = first.head().map(|h| h.clone());
-    match head {
-      Some(Symbol::Cur(c)) => {
-        first = first.tail();
-        second = second.append(Symbol::Cur(c.clone()));
-      }
-      _ => {break}
-    }
-  }
-
-  match direction {
-    Dir::L => {(first, second)}
-    Dir::R => {(second, first)}
-  }
-}
-
-fn join_cursor(cur: Cursor, l: &List<Symbol>, r: &List<Symbol>)
-  -> Option<(List<Symbol>, List<Symbol>)> {
-  let mut first = l.clone();
-  let mut second = r.clone();
-
-  //search left
-  loop {
-    match first.head().map(|h| h.clone()) {
-      None => {break}
-      Some(Symbol::Data(d)) => {
-        first = first.tail();
-        second = second.append(Symbol::Data(d));
-      }
-      Some(Symbol::Cur(c)) => {
-        if cur == c {
-          return Some((first.tail(),second))
-        }else{
-          first = first.tail();
-          second = second.append(Symbol::Cur(c));
-        }
-      }
-    }
-  }
-  //search right
-  first = l.clone();
-  second = r.clone();
-  loop {
-    match second.head().map(|h| h.clone()) {
-      None => {return None}
-      Some(Symbol::Data(d)) => {
-        second = second.tail();
-        first = first.append(Symbol::Data(d));
-      }
-      Some(Symbol::Cur(c)) => {
-        if cur == c {
-          return Some((first,second.tail()))
-        }else{
-          second = second.tail();
-          first = first.append(Symbol::Cur(c));
-        }
-      }
-    }
-  }
-
-}
-
-
-// command list to content zipper
-fn cl_to_cz(commands: &List<Command>) -> CZip<Symbol> {
-  let mut before: List<Symbol> = List::new();
-  let mut ccursor: Cursor = "0".to_string();
-  let mut after: List<Symbol> = List::new();
-
-  for command in commands.iter() {
-    let (before2, ccursor2, after2) =
-    match *command {
-      Command::Ins(ref d, Dir::R) => {
-        let (b, a) = passthrough(Dir::R, before, after);
-        (b.append(Symbol::Data(d.clone())), ccursor, a)
-      }
-      Command::Ins(ref d, Dir::L) => {
-        let (b, a) = passthrough(Dir::L, before, after);
-        (b, ccursor, a.append(Symbol::Data(d.clone())))
-      }
-      Command::Rem(Dir::L) => {
-        let (b, a) = passthrough(Dir::L, before, after);
-        (b.tail(), ccursor, a)
-      }
-      Command::Rem(Dir::R) => {
-        let (b, a) = passthrough(Dir::R, before, after);
-        (b, ccursor, a.tail())
-      }
-      Command::Move(Dir::L) => {
-        let (b, a) = passthrough(Dir::L, before, after);
-        match b.head() {
-          None => {(List::new(), ccursor, a)}
-          Some(d) => {(b.tail(), ccursor, a.append(d.clone()))}
-        }
-      }
-      Command::Move(Dir::R) => {
-        let (b, a) = passthrough(Dir::R, before, after);
-        match a.head() {
-          None => {(b, ccursor, List::new())}
-          Some(d) => {(b.append(d.clone()), ccursor, a.tail())}
-        }
-      }
-      Command::Ovr(ref d, Dir::L) => {
-        let (b, a) = passthrough(Dir::L, before, after);
-        (b.tail(), ccursor, a.append(Symbol::Data(d.clone())))
-      }
-      Command::Ovr(ref d, Dir::R) => {
-        let (b, a) = passthrough(Dir::R, before, after);
-        (b.append(Symbol::Data(d.clone())), ccursor, a.tail())
-      }
-      Command::Mk(ref c) => {
-        (before.append(Symbol::Cur(c.clone())), ccursor, after)
-      }
-      Command::Switch(ref c) => {
-        let withcursor = before.append(Symbol::Cur(ccursor.clone()));
-        match join_cursor(c.clone(), &withcursor, &after) {
-          Some((b,a)) => {(b, c.clone(), a)}
-          None => {(before, ccursor, after)}
-        }
-      }
-      Command::Jmp(ref c) => {
-        match join_cursor(c.clone(), &before, &after) {
-          Some((b,a)) => {
-            let withcursor = b.append(Symbol::Cur(c.clone()));
-            (withcursor, ccursor, a)
-          }
-          None => {(before, ccursor, after)}
-        }
-      }
-      Command::Join(ref c) => {
-        match join_cursor(c.clone(), &before, &after) {
-          Some((b,a)) => {(b, c.clone(), a)}
-          None => {(before, ccursor, after)}
-        }
-      }
-    };
-
-    before = before2;
-    after = after2;
-    ccursor = ccursor2;
-  }
-
-  (before, ccursor, after)
-}
 
 fn makelines(before: &List<Symbol>, after: &List<Symbol>, addbar: bool, showcursors: bool) -> List<String> {
   let mut out: List<String> = List::new();
@@ -561,7 +302,7 @@ fn main() {
           //mac's command key registers as unknown on my machine
           Key::Unknown |
           Key::LCtrl |
-          Key::LAlt |
+          //Key::LAlt |
           Key::RCtrl |
           Key::RAlt => {
             command_key_down = false;
@@ -575,7 +316,7 @@ fn main() {
           //mac's command key registers as unknown on my machine
           Key::Unknown |
           Key::LCtrl |
-          Key::LAlt |
+          //Key::LAlt |
           Key::RCtrl |
           Key::RAlt => {
             command_key_down = true;
