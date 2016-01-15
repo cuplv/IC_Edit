fa3ee448dc11531645ac554c724c81fc1188ca86
Kyle Headley <kyle.headley@colorado.edu>
2016-01-15 14:23:12 -0700

    testing settings

--- data/timing.sh
+++ data/timing.sh
@@ -1,10 +1,12 @@
+export RUST_MIN_STACK = 10485760
+
 # output file
 FILE=timing.csv
 
 # test runner
-TESTER="cargo run --release -- test "
+TESTER="cargo run --release -- test -n"
 
-for start in 100 200 300 400 500 600 700 800 900; do
+for start in 1000 2000 3000 4000 5000 6000 7000 8000 9000; do
   for ver in "-r" "-a"; do 
     for samp in 1 2 3 4 5 6 7; do
       echo "Running: $ver @ $start, samp:$samp"
