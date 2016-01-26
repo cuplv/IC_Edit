82f84ae6bc86e5da7315aa61c5d7f611a93faaf0
Matthew Hammer <matthew.hammer@colorado.edu>
2016-01-25 19:10:39 -0500

    spoke too soon! IC-Edit's fast version does not manage cursors correctly

--- src/main.rs
+++ src/main.rs
@@ -105,7 +105,7 @@ fn rnd_inputs(num: u32, nc: bool) -> List<Action> {
   }
 
   let mut rnd_action = |rng: &mut ThreadRng|{//(&rng: Rng) -> Action {
-    match rng.gen_range(0, 100) {
+    match rng.gen_range(0, 200) {
       0 ... 19 => {Action::Cmd(Command::Ovr(rnd_char(rng), rnd_dir(rng)))}
       20 ... 69 => {Action::Cmd(Command::Ins(rnd_char(rng), rnd_dir(rng)))}
       70 ... 79 => {Action::Cmd(Command::Rem(rnd_dir(rng)))}
@@ -118,8 +118,8 @@ fn rnd_inputs(num: u32, nc: bool) -> List<Action> {
             Action::Cmd(Command::Mk((cursor_count - 1).to_string()))
           }
           1 => {Action::Cmd(Command::Switch(rnd_cursor(rng, cursor_count)))}
-          2 => {Action::Cmd(Command::Jmp(rnd_cursor(rng, cursor_count)))}
-          3 => {Action::Cmd(Command::Join(rnd_cursor(rng, cursor_count)))}
+          // 2 => {Action::Cmd(Command::Jmp(rnd_cursor(rng, cursor_count)))}
+          //          3 => {Action::Cmd(Command::Join(rnd_cursor(rng, cursor_count)))}
           4 => {Action::Redo}
           _ => {Action::Undo}
 
